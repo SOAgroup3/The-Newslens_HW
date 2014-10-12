254cc1064ae88a505bba989d782e3a9cceeee0a1
peggy <peggy@140-127-218-233.nuk.edu.tw>
2014-10-12 16:03:51 +0800

    v4(after rubocop - 1)

--- newsdigest.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'scarpers.rb'
-
-newsfound = Scarper::NewsLens.get_news
-newsfound.each do |x|
-  puts x
-end
--- scarpers.rb
+++ scarpers.rb
@@ -1,22 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
 module Scarper
+  # class for NewsLens
   class NewsLens
     LINK = 'http://www.thenewslens.com'
-    NEWS_XPATH = "//main[@class='HolyGrail-content']//div[@class='post-list-item']"
+    NEWS_XPATH = "//main[@class='HolyGrail-content']
+                  //div[@class='post-list-item']"
     def self.gets_news
       doc = gets_html(LINK)
       content = gets_content(doc)
-      titles = gets_titles(content)
+      gets_titles(content)
     end
-    def self.gets_html(url)			
-      doc = Nokogiri::HTML(open(url))
+    def self.gets_html(url)
+      Nokogiri::HTML(open(url))
     end
     def self.gets_content(doc)
-      news = doc.xpath(NEWS_XPATH)
+      doc.xpath(NEWS_XPATH)
     end
     def self.gets_titles(news)
-      news.map { |x| x.text }
+      news.map(&:text)
     end
   end
 end
